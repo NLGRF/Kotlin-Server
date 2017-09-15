28a55801ea72150a7632229fa29d42c312c30406
NLGRF <nongrace6654@gmail.com>
2017-09-15 20:43:10 +0700

    Application Module

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.features.CallLogging
+import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.netty.Netty
 import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
-import org.jetbrains.ktor.routing.routing
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, 9000) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!")
-            }
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello World!")
         }
     }
-    server.start(wait = true)
 }
+
+//modules = [ MainKt.main ]
